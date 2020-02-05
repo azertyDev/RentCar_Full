b91e415cf58d97e1e3dad870d3db0511c52214ed
akbarbob <akbarbobomurod@gmail.com>
2020-02-05 20:01:59 +0500

    connect to backend and data load

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+   super(props);
+    this.state={
+     data:[]
+   }
+  }
+  componentDidMount(){
+    axios({
+      method:'POST',
+      url:'/login',
+      body:{
+        email:"EatonSmith@gmail.com",
+        password:"123"
+      },
+    }).then(response=>{this.setState({data:[...response.data]}); console.log(response)})
+    .catch(err=>console.log('err', err))
+  }
+  render(){
+    return(
+      <div>
+           {
+             this.state.data.map(item=>(
+               <div>{item.name}</div>
+             ))
+           }
+      </div>
+    )
+  }
 }
 
 export default App;
